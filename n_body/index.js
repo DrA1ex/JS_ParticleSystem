bc94abdbd954620ab7d4202c12fc61691e0adc6d
Alexander <DrA1ex@users.noreply.github.com>
2022-09-24 18:33:40 +0500

    Initial n-body implementation

---
+++ n_body/index.js
@@ -0,0 +1,178 @@
+import {SpatialTree} from "./tree.js";
+
+const urlSearchParams = new URLSearchParams(window.location.search);
+const params = Object.fromEntries(urlSearchParams.entries());
+
+const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent) || window.orientation !== undefined;
+
+const MOUSE_POINT_RADIUS = 3;
+const PARTICLE_CNT = ~~params.particle_count || (isMobile ? 10000 : 20000);
+const FPS = ~~params.fps || 60;
+const G = Number.parseFloat(params.g) || 9;
+const ParticleG = G / PARTICLE_CNT * 10;
+const Resistance = Number.parseFloat(params.resistance) || 0.99;
+
+const canvas = document.getElementById("canvas");
+
+const rect = canvas.getBoundingClientRect();
+
+const CanvasWidth = rect.width;
+const CanvasHeight = rect.height;
+
+canvas.style.width = CanvasWidth + "px";
+canvas.style.height = CanvasHeight + "px";
+canvas.width = CanvasWidth;
+canvas.height = CanvasHeight;
+
+const ctx = canvas.getContext('2d');
+
+const imageData = ctx.createImageData(CanvasWidth, CanvasHeight);
+const imageWidth = imageData.width;
+const pixels = new Uint32Array(imageData.data.buffer);
+
+const MousePosition = {x: CanvasWidth / 2, y: CanvasHeight / 2};
+const Particles = new Array(PARTICLE_CNT);
+
+function init() {
+    for (let i = 0; i < PARTICLE_CNT; i++) {
+        Particles[i] = {
+            x: Math.random() * CanvasWidth,
+            y: Math.random() * CanvasHeight,
+            velX: 0, velY: 0
+        };
+    }
+
+    canvas.onmousemove = canvas.ontouchmove = (e) => {
+        const point = e.touches ? e.touches[0] : e
+        const bcr = e.target.getBoundingClientRect();
+
+        MousePosition.x = point.clientX - bcr.x;
+        MousePosition.y = point.clientY - bcr.y;
+
+        e.preventDefault();
+    }
+}
+
+function calculateForce(p1, p2, g) {
+    const dx = p1.x - p2.x,
+        dy = p1.y - p2.y;
+
+    const distSquare = dx * dx + dy * dy;
+
+    let force = 0;
+    if (distSquare >= 400) // A magic number represent min process distance
+    {
+        force = -g / distSquare;
+    }
+
+    return [dx * force, dy * force];
+}
+
+function animateParticle(particle, g, attractor) {
+    const [xForce, yForce] = calculateForce(particle, attractor, g);
+    particle.velX += xForce;
+    particle.velY += yForce;
+}
+
+function applyForce(leaf, force) {
+    const [xForce, yForce] = force;
+    for (let i = 0; i < leaf.data.length; i++) {
+        const particle = leaf.data[i];
+        particle.velX += xForce;
+        particle.velY += yForce;
+    }
+}
+
+function physicsStep(particle) {
+    particle.velX *= Resistance;
+    particle.velY *= Resistance;
+    particle.x += particle.velX;
+    particle.y += particle.velY;
+
+    if (particle.x > CanvasWidth)
+        particle.x -= CanvasWidth;
+    else if (particle.x < 0)
+        particle.x += CanvasWidth;
+
+    if (particle.y > CanvasHeight)
+        particle.y -= CanvasHeight;
+    else if (particle.y < 0)
+        particle.y += CanvasHeight;
+}
+
+function render() {
+    function _calculateTree(leaf) {
+        const blocks = leaf.children;
+        if (blocks.length > 0) {
+            for (let i = 0; i < blocks.length; i++) {
+                _calculateTree(blocks[i]);
+            }
+
+            // Apply force between blocks
+            for (let i = 0; i < blocks.length; i++) {
+                const attractor = blocks[i].boundaryRect.center();
+                const g = ParticleG * blocks[i].length;
+
+                for (let j = 0; j < blocks.length; j++) {
+                    if (i === j) continue;
+
+                    const force = calculateForce(blocks[j].boundaryRect.center(), attractor, g);
+                    applyForce(blocks[j], force);
+                }
+            }
+        } else {
+            for (let i = 0; i < leaf.data.length; i++) {
+                const attractor = leaf.data[i];
+                for (let j = 0; j < leaf.data.length; j++) {
+                    if (i === j) continue;
+                    animateParticle(leaf.data[j], ParticleG, attractor);
+                }
+            }
+        }
+    }
+
+    ctx.clearRect(0, 0, CanvasWidth, CanvasHeight);
+
+    for (let i = 0; i < pixels.length; i++) {
+        pixels[i] = 0;
+    }
+
+    const tree = new SpatialTree(Particles, 128, 8);
+    _calculateTree(tree.root);
+
+    for (let i = 0; i < Particles.length; i++) {
+        const particle = Particles[i];
+
+        //animateParticle(particle, G, MousePosition);
+        physicsStep(particle);
+
+        const xVelToColor = 125 + Math.floor(particle.velX * 20);
+        const yVelToColor = 125 + Math.floor(particle.velY * 20);
+        const overSpeedColor = Math.max(0, xVelToColor + yVelToColor - 255 * 2);
+        const index = (Math.floor(particle.x) + Math.floor(particle.y) * imageWidth);
+        pixels[index] = 0xff000000 | (xVelToColor & 0xff) << 16 | (yVelToColor & 0xff) << 8 | overSpeedColor & 0xff;
+    }
+
+    ctx.putImageData(imageData, 0, 0);
+
+    ctx.fillStyle = "red";
+    ctx.beginPath();
+    ctx.arc(MousePosition.x - MOUSE_POINT_RADIUS / 2, MousePosition.y - MOUSE_POINT_RADIUS / 2,
+        MOUSE_POINT_RADIUS, 0, Math.PI * 2);
+    ctx.fill();
+}
+
+const refreshTime = 1000 / FPS;
+let lastStepTime = 0;
+
+function step(timestamp) {
+    if (timestamp >= lastStepTime + refreshTime) {
+        lastStepTime = timestamp;
+        render();
+    }
+
+    requestAnimationFrame(step)
+}
+
+init();
+requestAnimationFrame(step);
---
+++ n_body/tree.js
@@ -0,0 +1,123 @@
+/**
+ * @typedef {{x:number, y:number}} Particle
+ */
+
+class BoundaryRect {
+    constructor(left, top, right, bottom) {
+        this.left = left;
+        this.top = top;
+        this.right = right;
+        this.bottom = bottom;
+
+        this._height = null;
+        this._width = null;
+    }
+
+    get width() {
+        if (this._width === null) {
+            this._width = this.right - this.left;
+        }
+        return this._width;
+    }
+
+    get height() {
+        if (this._height === null) {
+            this._height = this.bottom - this.top;
+        }
+        return this._height;
+    }
+
+    contains(particle) {
+        return particle.x >= this.left && particle.x <= this.right &&
+            particle.y >= this.top && particle.y <= this.bottom;
+    }
+
+    center() {
+        return {x: this.left + this.width / 2, y: this.top + this.height / 2};
+    }
+
+    static fromData(data) {
+        let minX = data[0].x, maxX = data[0].x,
+            minY = data[0].y, maxY = data[0].y;
+
+        for (let i = 1; i < data.length; i++) {
+            const x = data[i].x;
+            if (minX > x) minX = x
+            if (maxX < x) maxX = x
+
+            const y = data[i].y;
+            if (minY > y) minY = y;
+            if (maxY < y) maxY = y;
+        }
+
+        return new BoundaryRect(minX, minY, maxX, maxY);
+    }
+}
+
+
+class Leaf {
+    /**
+     * @param {Array<Particle>} data
+     * @param {number=1} depth
+     * @param {BoundaryRect|null} rect
+     */
+    constructor(data, depth = 1, rect = null) {
+        this.data = data;
+        this.depth = depth;
+        this.length = data.length;
+        this.children = [];
+        this.boundaryRect = rect || BoundaryRect.fromData(data);
+    }
+
+    appendChild(data, rect = null) {
+        const leaf = new Leaf(data, this.depth + 1, rect);
+        this.children.push(leaf);
+
+        return leaf;
+    }
+
+    filterByRect(rect) {
+        return this.data.filter(p => rect.contains(p));
+    }
+}
+
+export class SpatialTree {
+    /**
+     * @param {Array<Particle>} data
+     * @param {number} maxCount
+     * @param {number=4} divideFactor
+     */
+    constructor(data, maxCount, divideFactor = 2) {
+        data.sort((p1, p2) => p1.x - p2.x || p1.y - p2.y);
+
+        this.root = new Leaf(data);
+        this.maxCount = maxCount;
+        this.divideFactor = divideFactor;
+
+        this._populate(this.root, data)
+    }
+
+    _populate(current) {
+        if (current.length <= this.maxCount) {
+            return;
+        }
+
+        const boundary = current.boundaryRect;
+        const xStep = boundary.width / this.divideFactor;
+        const yStep = boundary.height / this.divideFactor;
+
+        for (let x = 0; x < this.divideFactor; x++) {
+            for (let y = 0; y < this.divideFactor; y++) {
+                const left = boundary.left + x * xStep
+                const top = boundary.top + y * yStep;
+                const filterRect = new BoundaryRect(left, top, left + xStep, top + yStep);
+
+                const rectData = current.filterByRect(filterRect);
+                if (rectData.length > 0) {
+                    const leaf = current.appendChild(rectData, filterRect);
+                    this._populate(leaf);
+                }
+            }
+        }
+    }
+}
